6510ece19fab34226bdafcf58a8ff4c5cd161706
andreckw <114355564+andreckw@users.noreply.github.com>
2024-10-16 11:06:58 -0300

    todo

--- RenanYokoya/obj/Debug/net8.0/RenanYokoya.AssemblyInfo.cs
+++ RenanYokoya/obj/Debug/net8.0/RenanYokoya.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RenanYokoya")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41f52d15bbc692555f2e10cbdf25d612330462e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("RenanYokoya")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RenanYokoya")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
