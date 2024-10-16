b5712a49554497609b50440fa0caf69644071c89
andreckw <114355564+andreckw@users.noreply.github.com>
2024-10-16 09:20:45 -0300

    BD

---
+++ RenanYokoya/Models/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿namespace RenanYokoya.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=AndreNadalin_RenanYokoya.db");
+    }
+}
